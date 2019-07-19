edf485e32f56a83dd6d35a59e2585eec1cda11a7
MD Shakir Hussain <MDShakirH@hcl.com>
2019-07-19 18:14:10 +0530

    added search page

--- cisco-drew/src/components/search-main/searchMain.js
+++ cisco-drew/src/components/search-main/searchMain.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import searchBg from '../../img/search-bg.png';
 
 class SearchMain extends Component {
     render() {
         return (
-            <div>
-                Search Main
+            <div className="search-container">
+                <img src={searchBg} className="search-bg-img"/>
+                <div className="search-section">
+                    <input type="text"/>
+                    <button className="btn">Search</button>
+                </div>
             </div>
         )
     }
